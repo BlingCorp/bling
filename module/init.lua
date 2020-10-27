8aa94da85f986a3739dfb997c3d3e2667e737228
Nooo37 <senpai.ackabar@gmail.com>
2020-10-27 21:47:15 +0100

    added tiled wallpaper module

--- module/init.lua
+++ module/init.lua
@@ -1,3 +1,4 @@
 return {
-    window_swallowing = require("bling.module.window_swallowing")
+    window_swallowing = require("bling.module.window_swallowing"),
+    tiled_wallpaper   = require("bling.module.tiled_wallpaper")
 }
