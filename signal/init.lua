9f56595360acbab5cd916acbad7197077ffb6eb0
HumblePresent <60856003+HumblePresent@users.noreply.github.com>
2021-04-19 21:26:10 -0500

    Old playerctl cli backend added as option

--- signal/init.lua
+++ signal/init.lua
@@ -1,1 +1,7 @@
-return {playerctl = require(... .. ".playerctl")}
+local beautiful = require("beautiful")
+
+if beautiful.playerctl_backend == "cli" then
+    return {playerctl = require(... .. ".playerctl_cli")}
+else
+    return {playerctl = require(... .. ".playerctl_lib")}
+end
