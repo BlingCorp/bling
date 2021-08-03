69077963520abedceab843a3480c084887cb9780
Ksaper <ofek4430@gmail.com>
2021-08-03 16:23:57 +0300

    Inital commit for window switcher widget

--- widget/init.lua
+++ widget/init.lua
@@ -1,4 +1,5 @@
 return {
     tag_preview = require(... .. ".tag_preview"),
-    task_preview = require(... .. ".task_preview")
+    task_preview = require(... .. ".task_preview"),
+    window_switcher = require(... .. ".window_switcher")
 }
