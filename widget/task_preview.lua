46282edf72a6a7cbe168338f93dc6a79979a61c0
contribuewwt <38278035+undefinedDarkness@users.noreply.github.com>
2021-08-11 21:55:41 +0100

    Task preview: more flexible layout control (#85)
    * Task preview: +much more flexible layout control
    
    * Added awful import
    
    * requested changes
    
    * fixup! requested changes
    
    * fixup! fixup! requested changes
    
    * Fix nil error
    
    Because what you are returning is a `wibox.widget`, instead of using `setup` you must set the `widget` property of the popup.
    
    * fix docs
    
    * fixup! fix docs
    
    * task_preview docs update
    
    Add lua support to code block
    
    * task_preview docs update
    
    Just some nit picky things
    
    Co-authored-by: gokul <33443763+JavaCafe01@users.noreply.github.com>

--- widget/task_preview.lua
+++ widget/task_preview.lua
@@ -5,6 +5,8 @@
 --      v   (boolean)
 --      c   (client)
 --
+
+local awful = require("awful")
 local wibox = require("wibox")
 local helpers = require(tostring(...):match(".*bling") .. ".helpers")
 local gears = require("gears")
@@ -12,8 +14,9 @@ local beautiful = require("beautiful")
 local dpi = beautiful.xresources.apply_dpi
 local cairo = require("lgi").cairo
 
-local function draw_widget(c, task_preview_box, screen_radius, widget_bg,
-                           widget_border_color, widget_border_width, margin)
+-- TODO: rename structure to something better?
+local function draw_widget(c, widget_template, screen_radius, widget_bg,
+    widget_border_color, widget_border_width, margin, widget_width, widget_height)
 
     if not pcall(function () return type(c.content) end) then return end
     local content = gears.surface(c.content)
@@ -25,99 +28,121 @@ local function draw_widget(c, task_preview_box, screen_radius, widget_bg,
     cr.operator = cairo.Operator.SOURCE
     cr:paint()
 
-    task_preview_box:setup{
-        {
+    local widget = wibox.widget{
+        (widget_template or {
             {
                 {
                     {
-                        image = gears.surface.load(c.icon),
-                        resize = true,
-                        forced_height = dpi(20),
-                        forced_width = dpi(20),
-                        widget = wibox.widget.imagebox
-                    },
-                    {
                         {
-                            markup = c.name,
-                            align = "center",
-                            widget = wibox.widget.textbox
-                        },
-                        left = dpi(4),
-                        right = dpi(4),
-                        widget = wibox.container.margin
-                    },
-                    layout = wibox.layout.align.horizontal
-                },
-                {
-                    {
-                        {
-                            image = gears.surface.load(img),
+                            id = 'icon_role',
                             resize = true,
-                            clip_shape = helpers.shape.rrect(screen_radius),
+                            forced_height = dpi(20),
+                            forced_width = dpi(20),
                             widget = wibox.widget.imagebox
                         },
-                        valign = "center",
-                        halign = "center",
-                        widget = wibox.container.place
+                        {
+                            {
+                                id = 'name_role',
+                                align = "center",
+                                widget = wibox.widget.textbox
+                            },
+                            left = dpi(4),
+                            right = dpi(4),
+                            widget = wibox.container.margin
+                        },
+                        layout = wibox.layout.align.horizontal
                     },
-                    top = margin * 0.25,
-                    widget = wibox.container.margin
+                    {
+                        {
+                            {
+                                id = 'image_role',
+                                resize = true,
+                                clip_shape = helpers.shape.rrect(screen_radius),
+                                widget = wibox.widget.imagebox
+                            },
+                            valign = "center",
+                            halign = "center",
+                            widget = wibox.container.place
+                        },
+                        top = margin * 0.25,
+                        widget = wibox.container.margin
+                    },
+                    fill_space = true,
+                    layout = wibox.layout.fixed.vertical
                 },
-                fill_space = true,
-                layout = wibox.layout.fixed.vertical
+                margins = margin,
+                widget = wibox.container.margin
             },
-            margins = margin,
-            widget = wibox.container.margin
-        },
-        bg = widget_bg,
-        shape_border_width = widget_border_width,
-        shape_border_color = widget_border_color,
-        shape = helpers.shape.rrect(screen_radius),
-        widget = wibox.container.background
-    }
-end
-
-local enable = function(opts)
-
-    local opts = opts or {}
-
-    local widget_x = opts.x or dpi(20)
-    local widget_y = opts.y or dpi(20)
-    local widget_height = opts.height or dpi(200)
-    local widget_width = opts.width or dpi(200)
-    local placement_fn = opts.placement_fn or nil
-
-    local margin = beautiful.task_preview_widget_margin or dpi(0)
-    local screen_radius = beautiful.task_preview_widget_border_radius or dpi(0)
-    local widget_bg = beautiful.task_preview_widget_bg or "#000000"
-    local widget_border_color = beautiful.task_preview_widget_border_color or
-                                    "#ffffff"
-    local widget_border_width = beautiful.task_preview_widget_border_width or
-                                    dpi(3)
-
-    local task_preview_box = wibox({
-        type = "dropdown_menu",
-        visible = false,
-        ontop = true,
-        input_passthrough = true,
+            bg = widget_bg,
+            shape_border_width = widget_border_width,
+            shape_border_color = widget_border_color,
+            shape = helpers.shape.rrect(screen_radius),
+            widget = wibox.container.background
+        }),
+        widget = wibox.container.constraint,
         width = widget_width,
-        height = widget_height,
-        bg = "#00000000"
-    })
+        height = widget_height
+    }
 
-    awesome.connect_signal("bling::task_preview::visibility", function(s, v, c)
-        draw_widget(c, task_preview_box, screen_radius, widget_bg,
-                    widget_border_color, widget_border_width, margin)
+        -- todo: have something like a create callback here?
 
-        if placement_fn then
-            placement_fn(task_preview_box)
-        else
-            task_preview_box.x = s.geometry.x + widget_x
-            task_preview_box.y = s.geometry.y + widget_y
+        for _, w in ipairs(widget:get_children_by_id("image_role")) do
+            w.image = img -- todo: copy it with gears.surface.xxx or something
         end
 
-        task_preview_box.visible = v
-    end)
-end
+        for _, w in ipairs(widget:get_children_by_id("name_role")) do
+            w.text = c.name
+        end
 
-return {enable = enable}
+        for _, w in ipairs(widget:get_children_by_id("icon_role")) do
+            w.image = c.icon -- todo: detect clienticon
+        end
+
+        return widget
+    end
+
+    local enable = function(opts)
+
+        local opts = opts or {}
+
+        local widget_x = opts.x or dpi(20)
+        local widget_y = opts.y or dpi(20)
+        local widget_height = opts.height or dpi(200)
+        local widget_width = opts.width or dpi(200)
+        local placement_fn = opts.placement_fn or nil
+
+        local margin = beautiful.task_preview_widget_margin or dpi(0)
+        local screen_radius = beautiful.task_preview_widget_border_radius or dpi(0)
+        local widget_bg = beautiful.task_preview_widget_bg or "#000000"
+        local widget_border_color = beautiful.task_preview_widget_border_color or
+        "#ffffff"
+        local widget_border_width = beautiful.task_preview_widget_border_width or
+        dpi(3)
+
+        local task_preview_box = awful.popup({
+                    type = "dropdown_menu",
+                    visible = false,
+                    ontop = true,
+                    placement = placement_fn,
+                    widget = wibox.container.background, -- A dummy widget to make awful.popup not scream
+                    input_passthrough = true,
+                    bg = "#00000000"
+                })
+
+        awesome.connect_signal("bling::task_preview::visibility", function(s, v, c)
+            if v then 
+                -- Update task preview contents
+                task_preview_box.widget = draw_widget(c, opts.structure, screen_radius, widget_bg,
+                        widget_border_color, widget_border_width, margin, widget_width, widget_height)
+            end
+
+            if not placement_fn then
+                task_preview_box.x = s.geometry.x + widget_x
+                task_preview_box.y = s.geometry.y + widget_y
+            end
+
+            task_preview_box.visible = v
+        end)
+    end
+
+    return {enable = enable}
