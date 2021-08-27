2f69503610a9c9ed62ba6c2c3357d857f7713a56
undefinedDarkness <davidnoronha@outlook.in>
2021-08-27 09:59:42 +0530

    work

--- widget/tabbed_misc/titlebar_indicator.lua
+++ widget/tabbed_misc/titlebar_indicator.lua
@@ -1,7 +1,7 @@
-local wibox = require('wibox')
-local awful = require('awful')
-local gears = require('gears')
-local beautiful = require('beautiful')
+local wibox = require("wibox")
+local awful = require("awful")
+local gears = require("gears")
+local beautiful = require("beautiful")
 local dpi = require("beautiful.xresources").apply_dpi
 local tabbed_module = require(tostring(...):match(".*bling") .. ".module.tabbed")
 
@@ -17,7 +17,6 @@ end
 
 -- Needs to be run, every time a new titlbear is created
 return function(c, opts)
-
     -- Args & Fallback -- Widget templates are in their original loactions
     opts = gears.table.crush({
         layout_spacing = dpi(4),
@@ -25,12 +24,18 @@ return function(c, opts)
         icon_margin = dpi(4),
         bg_color_focus = "#ff0000",
         bg_color = "#00000000",
-        icon_shape = function(cr,w,h) gears.shape.rounded_rect(cr,w,h,0) end
-    }, gears.table.join(opts, beautiful.bling_tabbed_misc_titlebar_indicator)) 
+        icon_shape = function(cr, w, h)
+            gears.shape.rounded_rect(cr, w, h, 0)
+        end,
+        layout = wibox.layout.fixed.horizontal
+    }, gears.table.join(
+        opts,
+        beautiful.bling_tabbed_misc_titlebar_indicator
+    ))
 
     -- Container to store icons
     local tabbed_icons = wibox.widget({
-        layout = wibox.layout.fixed.horizontal,
+        layout = opts.layout,
         spacing = opts.layout_spacing,
     })
 
@@ -42,7 +47,7 @@ return function(c, opts)
             end
         end
 
-        -- Empty list 
+        -- Empty list
         if removed_c == c then
             tabbed_icons:reset()
         end
@@ -52,7 +57,7 @@ return function(c, opts)
         if tbl_contains(group.clients, c) then
             tabbed_icons:reset()
             local focused = group.clients[group.focused_idx]
-        
+
             -- Autohide?
             if #group.clients == 1 then
                 return
@@ -63,7 +68,7 @@ return function(c, opts)
                     {
                         {
                             {
-                                id = 'icon_role',
+                                id = "icon_role",
                                 forced_width = opts.icon_size,
                                 forced_height = opts.icon_size,
                                 widget = awful.widget.clienticon,
@@ -71,15 +76,20 @@ return function(c, opts)
                             margins = opts.icon_margin,
                             widget = wibox.container.margin,
                         },
-                        bg = (client == focused) and (opts.bg_color_focus) or (opts.bg_color),
+                        bg = (client == focused) and opts.bg_color_focus or opts.bg_color,
                         shape = opts.icon_shape,
-                        id = 'click_role',
+                        id = "click_role",
                         widget = wibox.container.background,
                     },
                     halign = "center",
                     valign = "center",
                     widget = wibox.container.place,
                 })
+
+                -- No creation call back since this would be called on creation & every time the widget updated.
+                if opts.widget_template.update_callback then
+                    opts.widget_template.update_callback(widget, client, group)
+                end
 
                 -- Add icons & etc
                 for _, w in ipairs(widget:get_children_by_id("icon_role")) do
@@ -90,7 +100,7 @@ return function(c, opts)
 
                 for _, w in ipairs(widget:get_children_by_id("click_role")) do
                     w:add_button(awful.button({}, 1, function()
-                        tabbed_module.switch_to(group,idx)
+                        tabbed_module.switch_to(group, idx)
                     end))
                 end
 
@@ -101,7 +111,6 @@ return function(c, opts)
 
     awesome.connect_signal("bling::tabbed::client_added", recreate)
     awesome.connect_signal("bling::tabbed::changed_focus", recreate)
-    
+
     return tabbed_icons
 end
-
--- widget/task_preview.lua
+++ widget/task_preview.lua
@@ -14,11 +14,22 @@ local dpi = beautiful.xresources.apply_dpi
 local cairo = require("lgi").cairo
 
 -- TODO: rename structure to something better?
-local function draw_widget(c, widget_template, screen_radius, widget_bg,
-                           widget_border_color, widget_border_width, margin,
-                           widget_width, widget_height)
-
-    if not pcall(function() return type(c.content) end) then return end
+local function draw_widget(
+    c,
+    widget_template,
+    screen_radius,
+    widget_bg,
+    widget_border_color,
+    widget_border_width,
+    margin,
+    widget_width,
+    widget_height
+)
+    if not pcall(function()
+        return type(c.content)
+    end) then
+        return
+    end
     local content = gears.surface(c.content)
     local cr = cairo.Context(content)
     local x, y, w, h = cr:clip_extents()
@@ -28,63 +39,60 @@ local function draw_widget(c, widget_template, screen_radius, widget_bg,
     cr.operator = cairo.Operator.SOURCE
     cr:paint()
 
-    local widget = wibox.widget {
-        (widget_template or {
+    local widget = wibox.widget(widget_template or {
             {
                 {
                     {
                         {
-                            id = 'icon_role',
+                            id = "icon_role",
                             resize = true,
                             forced_height = dpi(20),
                             forced_width = dpi(20),
-                            widget = wibox.widget.imagebox
+                            widget = wibox.widget.imagebox,
                         },
                         {
                             {
-                                id = 'name_role',
+                                id = "name_role",
                                 align = "center",
-                                widget = wibox.widget.textbox
+                                widget = wibox.widget.textbox,
                             },
                             left = dpi(4),
                             right = dpi(4),
-                            widget = wibox.container.margin
+                            widget = wibox.container.margin,
                         },
-                        layout = wibox.layout.align.horizontal
+                        layout = wibox.layout.align.horizontal,
                     },
                     {
                         {
                             {
-                                id = 'image_role',
+                                id = "image_role",
                                 resize = true,
                                 clip_shape = helpers.shape.rrect(screen_radius),
-                                widget = wibox.widget.imagebox
+                                widget = wibox.widget.imagebox,
                             },
                             valign = "center",
                             halign = "center",
-                            widget = wibox.container.place
+                            widget = wibox.container.place,
                         },
                         top = margin * 0.25,
-                        widget = wibox.container.margin
+                        widget = wibox.container.margin,
                     },
                     fill_space = true,
-                    layout = wibox.layout.fixed.vertical
+                    layout = wibox.layout.fixed.vertical,
                 },
                 margins = margin,
-                widget = wibox.container.margin
+                widget = wibox.container.margin,
             },
             bg = widget_bg,
             shape_border_width = widget_border_width,
             shape_border_color = widget_border_color,
             shape = helpers.shape.rrect(screen_radius),
-            widget = wibox.container.background
-        }),
-        width = widget_width,
-        height = widget_height,
-        widget = wibox.container.constraint
-    }
+            widget = wibox.container.background,
+        })
 
-    -- TODO: have something like a create callback here?
+    if widget_template.update_callback then
+        widget_template.update_callback(widget, c)
+    end
 
     for _, w in ipairs(widget:get_children_by_id("image_role")) do
         w.image = img -- TODO: copy it with gears.surface.xxx or something
@@ -98,11 +106,15 @@ local function draw_widget(c, widget_template, screen_radius, widget_bg,
         w.image = c.icon -- TODO: detect clienticon
     end
 
-    return widget
+    return wibox.widget {
+        widget,
+        widget = wibox.container.constraint,
+        height = widget_height,
+        width = widget_width
+    }
 end
 
 local enable = function(opts)
-
     local opts = opts or {}
 
     local widget_x = opts.x or dpi(20)
@@ -114,30 +126,33 @@ local enable = function(opts)
     local margin = beautiful.task_preview_widget_margin or dpi(0)
     local screen_radius = beautiful.task_preview_widget_border_radius or dpi(0)
     local widget_bg = beautiful.task_preview_widget_bg or "#000000"
-    local widget_border_color = beautiful.task_preview_widget_border_color or
-                                    "#ffffff"
-    local widget_border_width = beautiful.task_preview_widget_border_width or
-                                    dpi(3)
+    local widget_border_color = beautiful.task_preview_widget_border_color or "#ffffff"
+    local widget_border_width = beautiful.task_preview_widget_border_width or dpi(3)
 
-    local task_preview_box = awful.popup(
-                                 {
-            type = "dropdown_menu",
-            visible = false,
-            ontop = true,
-            placement = placement_fn,
-            widget = wibox.container.background, -- A dummy widget to make awful.popup not scream
-            input_passthrough = true,
-            bg = "#00000000"
-        })
+    local task_preview_box = awful.popup({
+        type = "dropdown_menu",
+        visible = false,
+        ontop = true,
+        placement = placement_fn,
+        widget = wibox.container.background, -- A dummy widget to make awful.popup not scream
+        input_passthrough = true,
+        bg = "#00000000",
+    })
 
     awesome.connect_signal("bling::task_preview::visibility", function(s, v, c)
         if v then
             -- Update task preview contents
-            task_preview_box.widget = draw_widget(c, opts.structure,
-                                                  screen_radius, widget_bg,
-                                                  widget_border_color,
-                                                  widget_border_width, margin,
-                                                  widget_width, widget_height)
+            task_preview_box.widget = draw_widget(
+                c,
+                opts.structure,
+                screen_radius,
+                widget_bg,
+                widget_border_color,
+                widget_border_width,
+                margin,
+                widget_width,
+                widget_height
+            )
         end
 
         if not placement_fn then
@@ -149,4 +164,4 @@ local enable = function(opts)
     end)
 end
 
-return {enable = enable}
+return { enable = enable }
