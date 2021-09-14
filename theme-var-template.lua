dfbf9f793ea3fe83c9f7a364964a2f95655f971f
Nooo37 <senpai.ackabar@gmail.com>
2021-09-14 20:23:34 +0200

    add all window switcher theme vars

--- theme-var-template.lua
+++ theme-var-template.lua
@@ -76,4 +76,25 @@ theme.task_preview_widget_border_color = "#ffffff" -- The border color of the wi
 theme.task_preview_widget_border_width = 3 -- The border width of the widget
 theme.task_preview_widget_margin = 0 -- The margin of the widget
 
+-- window switcher
+theme.window_switcher_widget_bg = "#000000" -- The bg color of the widget
+theme.window_switcher_widget_border_width = 3 -- The border width of the widget
+theme.window_switcher_widget_border_radius = 0 -- The border radius of the widget
+theme.window_switcher_widget_border_color = "#ffffff" -- The border color of the widget
+theme.window_switcher_clients_spacing = 20 -- The space between each client item
+theme.window_switcher_client_icon_horizontal_spacing = 5 -- The space between client icon and text
+theme.window_switcher_client_width = 150 -- The width of one client widget
+theme.window_switcher_client_height = 250 -- The height of one client widget
+theme.window_switcher_client_margins = 10 -- The margin between the content and the border of the widget
+theme.window_switcher_thumbnail_margins = 10 -- The margin between one client thumbnail and the rest of the widget
+theme.thumbnail_scale = false -- If set to true, the thumbnails fit policy will be set to "fit" instead of "auto"
+theme.window_switcher_name_margins = 10 -- The margin of one clients title to the rest of the widget
+theme.window_switcher_name_valign = "center" -- How to vertically align one clients title
+theme.window_switcher_name_forced_width = 200 -- The width of one title
+theme.window_switcher_name_font = "Sans 11" -- The font of all titles
+theme.window_switcher_name_normal_color = "#ffffff" -- The color of one title if the client is unfocused
+theme.window_switcher_name_focus_color = "#ff0000" -- The color of one title if the client is focused
+theme.window_switcher_icon_valign = "center" -- How to vertially align the one icon
+theme.window_switcher_icon_width = 40 -- Thw width of one icon
+
 -- LuaFormatter on
