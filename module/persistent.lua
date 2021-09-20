04725305f5cc458079e70e895bd4dd5ff5d18ae4
Ksaper <ofek4430@gmail.com>
2021-09-20 16:13:55 +0300

    Don't save the parent in the parent clients

--- module/persistent.lua
+++ module/persistent.lua
@@ -77,7 +77,9 @@ local function save()
             client_set_xproperty(client, "bling_tabbed_clients_amount", "number", #client.bling_tabbed.clients)
             client_set_xproperty(client, "bling_tabbed_focused_idx", "number", tostring(client.bling_tabbed.focused_idx))
             for index, bling_tabbed_client in ipairs(client.bling_tabbed.clients) do
-                client_set_xproperty(client, "bling_tabbed_client_" .. index, "string", tostring(bling_tabbed_client.window))
+                if bling_tabbed_client.window  ~= client.window then
+                    client_set_xproperty(client, "bling_tabbed_client_" .. index, "string", tostring(bling_tabbed_client.window))
+                end
             end
         else
             client_set_xproperty(client, "bling_tabbed_clients_amount", "number", 0)
