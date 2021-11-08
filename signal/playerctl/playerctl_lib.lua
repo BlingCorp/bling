4b92faa28a9956d98326aada04f3cc0dbe23ad4f
Kasper <ofek4430@gmail.com>
2021-11-08 13:24:24 +0200

    Update signal/playerctl/playerctl_lib.lua
    Co-authored-by: HumblePresent <60856003+HumblePresent@users.noreply.github.com>

--- signal/playerctl/playerctl_lib.lua
+++ signal/playerctl/playerctl_lib.lua
@@ -71,7 +71,7 @@ end
 
 function playerctl:pause()
     if self._private.manager.players[1] then
-        self._private.manager.players[1].pause(self._private.manager.players[1])
+        self._private.manager.players[1]:pause()
     end
 end
 
