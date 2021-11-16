a5ee0d525959a945d2bc19bfff50d6e28621212b
Ksaper <ofek4430@gmail.com>
2021-11-16 05:38:40 +0200

    Formatting and small improvements

--- module/scratchpad.lua
+++ module/scratchpad.lua
@@ -1,67 +1,94 @@
 local awful = require("awful")
 local gears = require("gears")
 local naughty = require("naughty")
-
-local ruled
-if awesome.version ~= "v4.3" then
-    ruled = require("ruled")
-end
-
 local helpers = require(tostring(...):match(".*bling") .. ".helpers")
+local capi = { awesome = awesome, client = client }
+local ruled = capi.awesome.version ~= "v4.3" and require("ruled") or nil
+local pairs = pairs
 
 local Scratchpad = { mt = {} }
 
---- Creates a new scratchpad object based on the argument
---
--- @param args A table of possible arguments
--- @return The new scratchpad object
-function Scratchpad:new(args)
-    args = args or {}
-    if args.awestore then
-        naughty.notify({
-            title = "Bling Error",
-            text = "Awestore is no longer supported! Please take a look at the scratchpad documentation and use rubato for animations instead.",
+--- Called when the turn off animation has ended
+local function on_animate_turn_off_end(self, c, anim, tag, turn_off_on_end)
+    anim:unsubscribe()
+    anim.ended:unsubscribe()
+
+    if turn_off_on_end then
+        -- When toggling off a scratchpad that's present on multiple tags
+        -- depsite still being unminizmied on the other tags it will become invisible
+        -- as it's position could be outside the screen from the animation
+        c:geometry({
+            x = self.geometry.x + c.screen.geometry.x,
+            y = self.geometry.y + c.screen.geometry.y,
+            width = self.geometry.width,
+            height = self.geometry.height,
+        })
+        helpers.client.turn_off(c, tag)
+
+        self:emit_signal("turn_off", c)
+
+        self.in_anim = false
+    end
+end
+
+--- The turn off animation
+local function animate_turn_off(self, c, anim, axis, turn_off_on_end)
+    local screen_on_toggled_scratchpad = c.screen
+    local tag_on_toggled_scratchpad = screen_on_toggled_scratchpad.selected_tag
+
+    if c.floating == false then
+        -- Save the client geometry before floating it
+        local non_floating_x = c.x
+        local non_floating_y = c.y
+        local non_floating_width = c.width
+        local non_floating_height = c.height
+
+        -- Can't animate non floating clients
+        c.floating = true
+
+        -- Set the client geometry back to what it was before floating it
+        c:geometry({
+            x = non_floating_x,
+            y = non_floating_y,
+            width = non_floating_width,
+            height = non_floating_height,
         })
     end
 
-    args.rubato = args.rubato or {}
-    args.in_anim = false
-    local ret = gears.object({})
-    gears.table.crush(ret, Scratchpad)
-    gears.table.crush(ret, args)
-    return ret
-end
 
---- Find all clients that satisfy the the rule
---
--- @return A list of all clients that satisfy the rule
-function Scratchpad:find()
-    return helpers.client.find(self.rule)
-end
+    if axis == "x" then
+        anim.pos = c.x
+    else
+        anim.pos = c.y
+    end
 
---- Applies the objects scratchpad properties to a given client
---
--- @param c A client to which to apply the properties
-function Scratchpad:apply(c)
-    if not c or not c.valid then
-        return
-    end
-    c.floating = self.floating
-    c.sticky = self.sticky
-    c.fullscreen = false
-    c.maximized = false
-    c:geometry({
-        x = self.geometry.x + awful.screen.focused().geometry.x,
-        y = self.geometry.y + awful.screen.focused().geometry.y,
-        width = self.geometry.width,
-        height = self.geometry.height,
-    })
-    if self.autoclose then
-        c:connect_signal("unfocus", function(c1)
-            c1.sticky = false -- client won't turn off if sticky
-            helpers.client.turn_off(c1)
-        end)
-    end
+    anim:subscribe(function(pos)
+        if c and c.valid then
+            if axis == "x" then
+                c.x = pos
+            else
+                c.y = pos
+            end
+        end
+        self.in_anim = true
+
+        -- Handles changing tag mid animation
+        -- Check for the following scenerio:
+        -- Toggle on scratchpad at tag 1
+        -- Toggle on scratchpad at tag 2
+        -- Toggle off scratchpad at tag 1
+        -- Switch to tag 2
+        -- Outcome: The client will remain on tag 1 and will instead be removed from tag 2
+        if screen_on_toggled_scratchpad.selected_tag ~= tag_on_toggled_scratchpad then
+            on_animate_turn_off_end(self, c, anim, tag_on_toggled_scratchpad, true)
+        end
+    end)
+
+    anim:set(anim:initial())
+
+    anim.ended:subscribe(function()
+        on_animate_turn_off_end(self, c, anim, nil, turn_off_on_end)
+    end)
 end
 
 --- The turn on animation
@@ -104,6 +131,61 @@ local function animate_turn_on(self, c, anim, axis)
     end)
 end
 
+--- Creates a new scratchpad object based on the argument
+--
+-- @param args A table of possible arguments
+-- @return The new scratchpad object
+function Scratchpad:new(args)
+    args = args or {}
+    if args.awestore then
+        naughty.notify({
+            title = "Bling Error",
+            text = "Awestore is no longer supported! Please take a look at the scratchpad documentation and use rubato for animations instead.",
+        })
+    end
+
+    args.rubato = args.rubato or {}
+    args.in_anim = false
+
+    local ret = gears.object{}
+    gears.table.crush(ret, Scratchpad)
+    gears.table.crush(ret, args)
+
+    return ret
+end
+
+--- Find all clients that satisfy the the rule
+--
+-- @return A list of all clients that satisfy the rule
+function Scratchpad:find()
+    return helpers.client.find(self.rule)
+end
+
+--- Applies the objects scratchpad properties to a given client
+--
+-- @param c A client to which to apply the properties
+function Scratchpad:apply(c)
+    if not c or not c.valid then
+        return
+    end
+    c.floating = self.floating
+    c.sticky = self.sticky
+    c.fullscreen = false
+    c.maximized = false
+    c:geometry({
+        x = self.geometry.x + awful.screen.focused().geometry.x,
+        y = self.geometry.y + awful.screen.focused().geometry.y,
+        width = self.geometry.width,
+        height = self.geometry.height,
+    })
+    if self.autoclose then
+        c:connect_signal("unfocus", function(c1)
+            c1.sticky = false -- client won't turn off if sticky
+            helpers.client.turn_off(c1)
+        end)
+    end
+end
+
 --- Turns the scratchpad on
 function Scratchpad:turn_on()
     local c = self:find()[1]
@@ -112,7 +194,7 @@ function Scratchpad:turn_on()
 
     if c and not self.in_anim and c.first_tag and c.first_tag.selected then
         c:raise()
-        client.focus = c
+        capi.client.focus = c
         return
     end
     if c and not self.in_anim then
@@ -205,89 +287,6 @@ function Scratchpad:turn_on()
     end
 end
 
---- Called when the turn off animation has ended
-local function on_animate_turn_off_end(self, c, anim, tag, turn_off_on_end)
-    anim:unsubscribe()
-    anim.ended:unsubscribe()
-
-    if turn_off_on_end then
-        -- When toggling off a scratchpad that's present on multiple tags
-        -- depsite still being unminizmied on the other tags it will become invisible
-        -- as it's position could be outside the screen from the animation
-        c:geometry({
-            x = self.geometry.x + c.screen.geometry.x,
-            y = self.geometry.y + c.screen.geometry.y,
-            width = self.geometry.width,
-            height = self.geometry.height,
-        })
-        helpers.client.turn_off(c, tag)
-
-        self:emit_signal("turn_off", c)
-
-        self.in_anim = false
-    end
-end
-
---- The turn off animation
-local function animate_turn_off(self, c, anim, axis, turn_off_on_end)
-    local screen_on_toggled_scratchpad = c.screen
-    local tag_on_toggled_scratchpad = screen_on_toggled_scratchpad.selected_tag
-
-    if c.floating == false then
-        -- Save the client geometry before floating it
-        local non_floating_x = c.x
-        local non_floating_y = c.y
-        local non_floating_width = c.width
-        local non_floating_height = c.height
-
-        -- Can't animate non floating clients
-        c.floating = true
-
-        -- Set the client geometry back to what it was before floating it
-        c:geometry({
-            x = non_floating_x,
-            y = non_floating_y,
-            width = non_floating_width,
-            height = non_floating_height,
-        })
-    end
-
-
-    if axis == "x" then
-        anim.pos = c.x
-    else
-        anim.pos = c.y
-    end
-
-    anim:subscribe(function(pos)
-        if c and c.valid then
-            if axis == "x" then
-                c.x = pos
-            else
-                c.y = pos
-            end
-        end
-        self.in_anim = true
-
-        -- Handles changing tag mid animation
-        -- Check for the following scenerio:
-        -- Toggle on scratchpad at tag 1
-        -- Toggle on scratchpad at tag 2
-        -- Toggle off scratchpad at tag 1
-        -- Switch to tag 2
-        -- Outcome: The client will remain on tag 1 and will instead be removed from tag 2
-        if screen_on_toggled_scratchpad.selected_tag ~= tag_on_toggled_scratchpad then
-            on_animate_turn_off_end(self, c, anim, tag_on_toggled_scratchpad, true)
-        end
-    end)
-
-    anim:set(anim:initial())
-
-    anim.ended:subscribe(function()
-        on_animate_turn_off_end(self, c, anim, nil, turn_off_on_end)
-    end)
-end
-
 --- Turns the scratchpad off
 function Scratchpad:turn_off()
     local c = self:find()[1]
@@ -335,8 +334,8 @@ function Scratchpad:toggle()
             end
         end
     else
-        is_turn_off = client.focus
-            and awful.rules.match(client.focus, self.rule)
+        is_turn_off = capi.client.focus
+            and awful.rules.match(capi.client.focus, self.rule)
     end
 
     if is_turn_off then
